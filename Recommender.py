f06259a48b4210d29625b9f39102405fbc52f816
sodajuice-jpeg <71705133+sodajuice-jpeg@users.noreply.github.com>
2024-05-03 22:12:39 -0400

    Update Recommender.py

--- Recommender.py
+++ Recommender.py
@@ -11,9 +11,10 @@ class Recommender:
         self.associations = {}
 
     def loadBooks(self):
-        filepath = None
-        while not os.path.exists(filepath):
+        while True:
             filepath = filedialog.askopenfilename(title="Please select a book file.", initialdir=os.getcwd())
+            if os.path.exists(filepath):
+                break
         book_file = open(filepath, "r")
         for line in book_file:
             line = line.strip()
@@ -23,4 +24,3 @@ class Recommender:
                                 line_list[9], line_list[10])
             self.books[line_list[0]] = book_project
         book_file.close()
-        
