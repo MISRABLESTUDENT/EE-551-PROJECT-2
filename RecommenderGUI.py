7c612a46daef9ef6f530d9c8828369ef80d71e71
JunchengZhou23 <162488134+JunchengZhou23@users.noreply.github.com>
2024-05-05 21:11:05 -0400

    Add files via upload

--- RecommenderGUI.py
+++ RecommenderGUI.py
@@ -6,8 +6,9 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
 from Recommender import Recommender as rc
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-#import Recommender as rc
 
 class RecommenderGUI:
     def __init__(self):
@@ -23,21 +24,24 @@ class RecommenderGUI:
         self.buttonFrame = tk.Frame(self.main_window)
         self.buttonFrame.pack(side='bottom', fill='x', expand=False)
 
-        self.loadshowsButton = tk.Button(self.buttonFrame, text="Load Shows", command=self.loadShows) #command = rc.loadShows)
-        self.loadshowsButton.pack(side='left', padx=70)
+        self.loadshowsButton = tk.Button(self.buttonFrame, text="Load Shows", command=self.loadShows)   ##Load Shows
+        self.loadshowsButton.pack(side='left', padx=60)
         
-        #self.loadbooksButton = tk.Button(self.buttonFrame, text="Load Books", command=rc.loadBooks) #command = rc.loadBooks)
-        self.loadbooksButton = tk.Button(self.buttonFrame, text="Load Books", command=self.loadBooks)
-        self.loadbooksButton.pack(side='left', padx=70)
+
+        self.loadbooksButton = tk.Button(self.buttonFrame, text="Load Books", command=self.loadBooks)  ## Load Books
+        self.loadbooksButton.pack(side='left', padx=60)
         
-        self.loadrecommendationsButton = tk.Button(self.buttonFrame, text="Load Recommendations", command=self.loadAssociations) #command = rc.loadAssocitaions)
-        self.loadrecommendationsButton.pack(side='left', padx=70)
+        self.loadrecommendationsButton = tk.Button(self.buttonFrame, text="Load Recommendations", command=self.loadAssociations)
+        self.loadrecommendationsButton.pack(side='left', padx=60)    # Load Recommendations
         
-        self.informationButton = tk.Button(self.buttonFrame, text="Information", command=self.creditInfoBox)  #need to complete
-        self.informationButton.pack(side='left', padx=70)
+        self.informationButton = tk.Button(self.buttonFrame, text="Information", command=self.creditInfoBox) ## Information
+        self.informationButton.pack(side='left', padx=60)
+
+        self.ratingsButton = tk.Button(self.buttonFrame, text="Ratings",command=self.piecharts)  ## pie charts
+        self.ratingsButton.pack(side='left', padx=60)
         
         self.quitButton = tk.Button(self.buttonFrame, text="Quit", command=self.main_window.destroy)
-        self.quitButton.pack(side='left', padx=70)
+        self.quitButton.pack(side='left', padx=60)   ## Quit
             
 
         
@@ -45,56 +49,28 @@ class RecommenderGUI:
               
         self.movies_frame = ttk.Frame(notebook)
         notebook.add(self.movies_frame,text = 'Movies')
-        
-        self.label_movies = ttk.Label(self.movies_frame)
-        self.label_movies.pack(pady=20, padx=20)
+        self.moviewelcome = tk.Label(self.movies_frame, text='Please load show file')
+        self.moviewelcome.grid(row=0, column=0, padx=10, pady=10)
 
-        self.label_movies.smootTextArea = tk.Text(self.movies_frame, wrap=tk.WORD)
-        self.label_movies.smootTextArea.pack(side=tk.TOP)
-        #label_movies.smootInfo = ('need getmovielist and getmoviestats')
-        #label_movies.smootTextArea = tk.Text(label_movies, wrap=tk.WORD)
-        #label_movies.smootTextArea.insert(tk.END, label_movies.smootInfo)
-        #label_movies.smootTextArea.config(state=tk.DISABLED)
-        #label_movies.smootTextArea.pack(side=tk.TOP)
-        
-        #movie_listbox = tk.Listbox(self.movies_frame, width=50, height=10)
-        #movie_listbox.pack(padx=20, pady=20)
 
 
     ## tvshows tab##
-        
+
         self.tvshows_frame = ttk.Frame(notebook)
         notebook.add(self.tvshows_frame,text ='TV Shows')
-        
-        self.label_tvshows = ttk.Label(self.tvshows_frame)
-        self.label_tvshows.pack(pady=20, padx=20)
+        self.tvwelcome = tk.Label(self.tvshows_frame, text='Please load show file')
+        self.tvwelcome.grid(row=0, column=0, padx=10, pady=10)
 
-        self.label_tvshows.smootInfo = ('need gettvlist and gettvstats')
-        self.label_tvshows.smootTextArea = tk.Text(self.label_tvshows, wrap=tk.WORD)
-        self.label_tvshows.smootTextArea.insert(tk.END, self.label_tvshows.smootInfo)
-        self.label_tvshows.smootTextArea.config(state=tk.DISABLED)
-        self.label_tvshows.smootTextArea.pack(side=tk.TOP)
-        
-        tvshows_listbox = tk.Listbox(self.tvshows_frame, width=50, height=10)
-        tvshows_listbox.pack(padx=20, pady=20)
         
         
     ## books tab##
         
         self.books_frame = ttk.Frame(notebook)
         notebook.add(self.books_frame,text ='Books')
-        
-        self.label_books = ttk.Label(self.books_frame)
-        self.label_books.pack(pady=20, padx=20)
+        self.bookwelcome= tk.Label(self.books_frame, text='Please load book file')
+        self.bookwelcome.grid(row=0, column=0, padx=10, pady=10)
 
-        #label_books.smootInfo = ('need getbooklist and getbookstats')
-        #label_books.smootTextArea = tk.Text(label_books, wrap=tk.WORD)
-        #label_books.smootTextArea.insert(tk.END, label_books.smootInfo)
-        #label_books.smootTextArea.config(state=tk.DISABLED)
-        #label_books.smootTextArea.pack(side=tk.TOP)
-        
-        books_listbox = tk.Listbox(self.books_frame, width=50, height=10)
-        books_listbox.pack(padx=20, pady=20)
+
         
     ## search movies/tv tab ##
     
@@ -112,10 +88,11 @@ class RecommenderGUI:
         self.searchb_frame = ttk.Frame(notebook)
         notebook.add(self.searchb_frame,text ='Search Books')
         
-        label_searchb = ttk.Label(self.searchb_frame)
+        self.label_searchb = ttk.Label(self.searchb_frame)
+        self.searchb_result = 'You can search something here'
         self.searchb()
 
- 
+
         
         
     ## recommendations tab ##
@@ -126,63 +103,102 @@ class RecommenderGUI:
         self.getrc()
 
         
-    #def loadShows(self):
-     #   self.recommender.loadShows() 
+
     def loadShows(self):
         #try:
-        self.recommender.loadShows()  # 假设这个方法加载电影数据
+        self.recommender.loadShows()
         #print("Loaded shows:", self.recommender.shows)
-        self.update_movie_list()  # 更新电影列表显示
+        self.update_movie_list()
         self.update_tv_list()
-        #except Exception as e:
-         #   messagebox.showerror("Error", f"Failed to load shows: {str(e)}")
 
-            #messagebox.showerror(' ','load shows! function not implemented yet')
     def update_movie_list(self):
-        movie_list = self.recommender.getMovieList()  # 获取电影列表字符串
-        print("Movie list:", movie_list)  # 查看返回的电影列表内容
-        self.label_movies.smootTextArea.config(state=tk.NORMAL)  # 允许修改内容
-        self.label_movies.smootTextArea.delete('1.0', tk.END)  # 清空现有内容
-        self.label_movies.smootTextArea.insert(tk.END, movie_list)  # 插入新的电影列表
-        self.label_movies.smootTextArea.config(state=tk.DISABLED)  # 禁止修改内容
+        self.moviewelcome.destroy()
+
+        self.smootTextArea = tk.Text(self.movies_frame,height=20,width=200,state='normal')
+        self.smootTextArea.grid(row=6, column=0, columnspan=2, padx=0, pady=10, sticky='nsew')
+        self.smootTextArea.config(state=tk.NORMAL)
+        self.smootTextArea.delete('1.0', tk.END)
+        self.smootTextArea.insert(tk.END, self.recommender.getMovieList())
+        self.smootTextArea.config(state=tk.DISABLED)
+       ###ratings
+
+        self.moviesrating = tk.Text(self.movies_frame, height=30, width=200, state='normal')
+        self.moviesrating.grid(row=7, column=0, columnspan=2, padx=0, pady=10, sticky='nsew')
+        self.moviesrating.config(state=tk.NORMAL)
+        self.moviesrating.delete('1.0', tk.END)
+        self.moviesrating.insert(tk.END,self.recommender.getMovieStats()[0])
+        self.moviesrating.config(state=tk.DISABLED)
     def update_tv_list(self):
-        tv_list = self.recommender.getTVList()  # 获取电影列表字符串
-        print("TV shows list:", tv_list)  # 查看返回的电影列表内容
-        self.label_tvshows.smootTextArea.config(state=tk.NORMAL)  # 允许修改内容
-        self.label_tvshows.smootTextArea.delete('1.0', tk.END)  # 清空现有内容
-        self.label_tvshows.smootTextArea.insert(tk.END, tv_list)  # 插入新的电影列表
-        self.label_tvshows.smootTextArea.config(state=tk.DISABLED)  # 禁止修改内容
-        
+
+        self.tvwelcome.destroy()
+        #print("TV shows list:", tv_list)
+        self.TVTextArea = tk.Text(self.tvshows_frame,height=20,width=200,state='normal')
+        self.TVTextArea.grid(row=6, column=0, columnspan=2, padx=0, pady=10, sticky='nsew')
+        self.TVTextArea.delete('1.0', tk.END)
+        #print(self.recommender.shows)
+        self.TVTextArea.insert(tk.END, self.recommender.getTVList())
+        self.TVTextArea.config(state=tk.DISABLED)
+        ###ratings
+        self.TVrating = tk.Text(self.tvshows_frame, height=30, width=200, state='normal')
+        self.TVrating.grid(row=7, column=0, columnspan=2, padx=0, pady=10, sticky='nsew')
+        self.TVrating.config(state=tk.NORMAL)
+        self.TVrating.delete('1.0', tk.END)
+        self.TVrating.insert(tk.END, self.recommender.getTVStats()[0])
+        self.TVrating.config(state=tk.DISABLED)
     def update_books_list(self):
-        book_list = self.recommender.getBookList()  # 获取电影列表字符串
-        print("Book list:", book_list)  # 查看返回的电影列表内容
-        self.label_books.smootTextArea.config(state=tk.NORMAL)  # 允许修改内容
-        self.label_books.smootTextArea.delete('1.0', tk.END)  # 清空现有内容
-        self.label_books.smootTextArea.insert(tk.END, book_list)  # 插入新的电影列表
-        self.label_books.smootTextArea.config(state=tk.DISABLED)  # 禁止修改内容
+        ###########books list
+        self.bookwelcome.destroy()
+        self.Booklist = tk.Text(self.books_frame, height=20, width=200, state='normal')
+        self.Booklist.grid(row=6, column=0, columnspan=2, padx=0, pady=10, sticky='nsew')
+        self.Booklist.config(state=tk.NORMAL)
+        self.Booklist.delete('1.0', tk.END)
+        self.Booklist.insert(tk.END, self.recommender.getBookList())
+        self.Booklist.config(state=tk.DISABLED)
+        ####Average page ####
+        self.Booksrating = tk.Text(self.books_frame, height=30, width=200, state='normal')
+        self.Booksrating.grid(row=7, column=0, columnspan=2, padx=0, pady=10, sticky='nsew')
+        self.Booksrating.config(state=tk.NORMAL)  #
+        self.Booksrating.delete('1.0', tk.END)
+        self.Booksrating.insert(tk.END,self.recommender.getBookStats())
+        self.Booksrating.config(state=tk.DISABLED)
+
     def loadBooks(self):
         self.recommender.loadBooks() 
         self.update_books_list()
-    #def loadBooks(self):
-     #       messagebox.showerror(' ','load books! function not implemented yet')
+
             
     def loadAssociations(self):
         self.recommender.loadAssociations() 
-            #messagebox.showerror(' ','load recommendations! function not implemented yet')
-            
+
     def creditInfoBox(self):
-        message = 'Group members: Juncheng Zhou, Yujia Liu and Tianyu Sheng\n Completed on : May 7,2024'
+        message = 'Group members: Juncheng Zhou, Yujia Liu and Tianyu Sheng\n Completed on : May 5,2024'
         messagebox.showinfo('Credit Information',message)
-        
-    def searchShows(self):
-            messagebox.showerror(' ','searchshows! function not implemented yet')
-            
-    def searchBooks(self):
-            messagebox.showerror(' ','searchbooks! function not implemented yet')
-            
-    def getRecommendations(self):
-            messagebox.showinfo(' ', 'get recommendations! function not implemented yet') 
-   
+
+    def piecharts(self):
+        ratings_tab = ttk.Frame(ttk.Notebook(self.main_window))
+        movies_ratings = self.recommender.getMovieStats()[1]
+        tv_ratings = self.recommender.getTVStats()[1]
+
+
+        #first piechart
+        fig1, ax1 = plt.subplots()
+        ax1.pie(movies_ratings.values(), labels=movies_ratings.keys(), autopct='%1.2f%%', startangle=90)
+        ax1.axis('equal')
+
+        canvas = FigureCanvasTkAgg(fig1,master=ratings_tab)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+
+        ## second pie chart
+        fig2, ax2 = plt.subplots()
+        ax2.pie(tv_ratings.values(), labels=tv_ratings.keys(), autopct='%1.2f%%', startangle=90)
+        ax2.axis('equal')
+
+        canvas = FigureCanvasTkAgg(fig2, master=ratings_tab)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+
+
 
     ###search tv/movies
     def searchmt(self):
@@ -192,21 +208,18 @@ class RecommenderGUI:
         self.type_combobox = ttk.Combobox(self.searchmt_frame,values=['Movie','TV Show'])
         self.type_combobox.grid(row=0, column=0, padx=50, pady=10, sticky='w')
         self.type_combobox.set('Movie')
-        print("设置默认 show_type：", self.type_combobox.get())  # 验证设置是否成功
+
         # text entry#
         self.title = self.create_entry('Title:',1,self.searchmt_frame)
         self.director = self.create_entry("Director:", 2,self.searchmt_frame)
         self.actor = self.create_entry("Actor:", 3,self.searchmt_frame)
         self.genre = self.create_entry("Genre:", 4,self.searchmt_frame)
-        
+
 
         #research button
         self.search_button = ttk.Button(self.searchmt_frame, text="Search", command=self.perform_search_mt)
         self.search_button.grid(row=5, column=0, columnspan=2, pady=10, sticky='w')
-        
-        #result shown#
-        self.result_text = tk.Text(self.searchmt_frame, state='disabled')
-        self.result_text.grid(row=6, column=0, columnspan=2, padx=0, pady=10, sticky='nsew')
+
    
 
     ###search books
@@ -219,13 +232,14 @@ class RecommenderGUI:
         title= self.Title.get()
         publisher= self.Publisher.get()
         author= self.Author.get()
+
+
+
         #research button
-        self.search_button = ttk.Button(self.searchb_frame, text="Search", command=lambda:self.perform_search_b(title,author, publisher))
+        self.search_button = ttk.Button(self.searchb_frame, text="Search", command=self.perform_search_b)
         self.search_button.grid(row=5, column=0, columnspan=2, pady=10, sticky='w')
         
-        #result shown#
-        self.result_text = tk.Text(self.searchb_frame, state='disabled')
-        self.result_text.grid(row=6, column=0, columnspan=2, padx=0, pady=10, sticky='nsew')
+
     
     
     
@@ -236,33 +250,41 @@ class RecommenderGUI:
         entry.grid(row=row, column=0, padx=len(label_text*7), pady=5, sticky='w')
         return entry
     
-    def perform_search_b(self,title, authors, publisher):
-        #messagebox.showinfo('Warning','function unfinished')
-        self.recommender.searchBooks(title, authors, publisher)
+    def perform_search_b(self):
+        title = self.Title.get()
+        author = self.Author.get()
+        publisher = self.Publisher.get()
+        self.searchb_result = self.recommender.searchBooks(title, author, publisher)
+        self.result_text = tk.Text(self.searchb_frame, height=30,width=200,state='normal')
+        self.result_text.grid(row=6, column=0, columnspan=2, padx=0, pady=10, sticky='nsew')
+
+        self.result_text.delete('1.0', tk.END)
+        self.result_text.insert(tk.END, self.searchb_result)
+        self.result_text.config(state='disabled')
     def perform_search_mt(self):
-        # 获取组合框中的值
-        self.main_window.update()  # 更新 GUI 状态
         show_type = self.type_combobox.get()
-        print("检索到的 show_type：", show_type)  # 打印检索到的 show_type 值
         title = self.title.get()
         director = self.director.get()
         actor = self.actor.get()
         genre = self.genre.get()
-
-        print('good luck', show_type, title, director, actor, genre)
-
-        self.recommender.searchTVMovies(show_type, title, director, actor, genre)
-        
+        #print('good luck', show_type, title, director, actor, genre)
+        self.searchmt_result=self.recommender.searchTVMovies(show_type, title, director, actor, genre)
+        self.result_text_mt = tk.Text(self.searchmt_frame, height=30, width=200, state='normal')
+        self.result_text_mt.grid(row=6, column=0, columnspan=2, padx=0, pady=10, sticky='nsew')
+        self.result_text.delete('1.0', tk.END)
+        #print(self.searchmt_result)
+        self.result_text_mt.insert(tk.END, self.searchmt_result)
+        self.result_text_mt.config(state='disabled')
     #get recommendations##
     def getrc(self):
         ## Movie, TV Show or Book
         self.type = ttk.Label(self.recommendations_frame,text='Type')
         self.type.grid(row=0, column=0, padx=0, pady=10, sticky='w')
-        self.type_combobox = ttk.Combobox(self.recommendations_frame,values=['Movie','TV Show','Book'])
-        self.type_combobox.grid(row=0, column=0, padx=50, pady=10, sticky='w')
+        self.type_combobox3 = ttk.Combobox(self.recommendations_frame,values=['Movie','TV Show','Book'])
+        self.type_combobox3.grid(row=0, column=0, padx=50, pady=10, sticky='w')
     ## search title
         # text entry#
-        self.Title = self.create_entry('Title:',1,self.recommendations_frame)
+        self.Title3 = self.create_entry('Title:',1,self.recommendations_frame)
         
         #research button
         self.search_button = ttk.Button(self.recommendations_frame, text="Get Recommendation", command=self.getRecommendations) #####
@@ -271,7 +293,20 @@ class RecommenderGUI:
         #result shown#
         self.result_text = tk.Text(self.recommendations_frame, state='disabled')
         self.result_text.grid(row=6, column=0, columnspan=2, padx=0, pady=10, sticky='nsew')
-    
+
+    def getRecommendations(self):
+            #messagebox.showinfo(' ', 'get recommendations! function not implemented yet')
+            title = self.Title3.get()
+            type1 = self.type_combobox3.get()
+            self.getrc_result = self.recommender.getRecommendations(type1, title)
+            self.result_text_mt = tk.Text(self.recommendations_frame, height=30, width=200, state='normal')
+            self.result_text_mt.grid(row=6, column=0, columnspan=2, padx=0, pady=10, sticky='nsew')
+            self.result_text.delete('1.0', tk.END)
+            #print(self.getrc_result)
+            self.result_text_mt.insert(tk.END, self.getrc_result)
+            self.result_text_mt.config(state='disabled')
+
+
 def main():
     app = RecommenderGUI()
     app.main_window.mainloop()
